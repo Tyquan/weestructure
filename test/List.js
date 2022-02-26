b6d52c74570c3f661ad10f75793e2bc26d37103c
Tyquan Reddick <Tyquanjreddick@gmail.com>
2022-02-25 22:14:48 -0500

    refactored

--- test/List.js
+++ test/List.js
@@ -4,25 +4,24 @@ const List = require('../libs/List');
 
 describe("List Module", () => {
 
+    const numberList = new List([1,2,3,4,5]);
+    const emptyList = new List();
+
     describe("List is a class object", () => {
         it("should be equal to a class not {}", () => {
             assert.notEqual(List, {});
         });
         it("should have a dataStore property that takes a starting array by default", () => {
-            const list = new List([1,2,3,4,5]);
-            assert.deepEqual(list.dataStore, [1,2,3,4,5]);
+            assert.deepEqual(numberList.dataStore, [1,2,3,4,5]);
         });
         it("should have a dataStore property that returns [] if no startingArray is given by default", () => {
-            const list = new List();
-            assert.deepEqual(list.dataStore, []);
+            assert.deepEqual(emptyList.dataStore, []);
         });
         it("should have a listSize property set to dataStore.length by default", () => {
-            const list = new List();
-            assert.deepEqual(list.listSize, list.dataStore.length);
+            assert.deepEqual(numberList.listSize, numberList.dataStore.length);
         });
         it("should have a position property set to 0 by default", () => {
-            const list = new List();
-            assert.deepEqual(list.position, 0);
+            assert.deepEqual(emptyList.position, 0);
         });
     });
 
